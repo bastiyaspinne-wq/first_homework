090f2ddcc56e876e9538885cfb6418c243027ad0
bastiyaspinne-wq <bastiyaspinne@gmail.com>
2026-03-30 17:17:22 +0300

    i did home work 7.4

--- task_7.4.py
+++ task_7.4.py
@@ -1,5 +1,7 @@
 def common_elements():
     list_3 = set(range(0, 100, 3))
     list_5 = set(range(0, 100, 5))
-    common = list_3.intersection(list_5)
-    return common
+    result = list_3.intersection(list_5)
+    return result
+assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print('OK')
