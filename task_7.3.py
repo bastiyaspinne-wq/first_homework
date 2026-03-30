c60c26051a26ec9020fb66717c9ae1f0cf33e1ab
bastiyaspinne-wq <bastiyaspinne@gmail.com>
2026-03-30 17:02:39 +0300

    i did home work 6 and start 7

---
+++ task_7.3.py
@@ -0,0 +1,11 @@
+def second_index(text: str, symbol: str) -> int or None:
+    first = text.find(symbol)
+    if first == -1:
+        return None
+    second = text.find(symbol, first + len(symbol))
+    return second if second != -1 else None
+assert second_index('sims', 's') == 3, 'Test1'
+assert second_index('find the river', 'e') == 12, 'Test2'
+assert second_index('hi', 'h') is None, 'Test3'
+assert second_index('Hello, hello', 'lo') == 10, 'Test4'
+print('OK')
